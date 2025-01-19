a67ac16b0be9cfc0d415a4209992bc23ac9b85d5
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-01-20 03:58:04 +0600

    protected navlinks added

--- src/pages/Dashboard/leftside/Sidebar.jsx
+++ src/pages/Dashboard/leftside/Sidebar.jsx
@@ -5,11 +5,13 @@ import { useState } from "react";
 import { GrLogout } from "react-icons/gr";
 import logo from '../../../assets/logo.png'
 import { toast } from "react-toastify";
+import useRole from "../../../hooks/useRole";
 
 
 const Sidebar = () => {
     const { user, logoutUser } = useAuth()
     const [isActive, setActive] = useState(false)
+    const [role, isLoading] = useRole()
     //responsive hander 
     const handleToggle = () => {
         setActive(!isActive)
@@ -60,24 +62,31 @@ const Sidebar = () => {
                         {/* Nav Items */}
                         <div className='flex flex-col justify-between flex-1 mt-6'>
                             <ul className=" list-none">
-                                <li className=' px-4 py-2 hover:bg-slate-300 my-1'>
+                                {/* only Admin */}
 
-                                    <NavLink className={(isActive) => isActive ? 'text-xl text-gray-700 ' : '  text-xl text-black '} to='/dashboard/all-employee-list'>All Employee</NavLink>
-                                </li>
-                                <li className=' px-4 py-2 hover:bg-slate-300 my-1'>
-                                    <NavLink className={(isActive) => isActive ? 'text-xl text-gray-700 ' : '  text-xl text-black '} to='/dashboard/payroll'>Pay Role</NavLink>
+                                {role === 'admin' && <>
+                                    <li className=' px-4 py-2 hover:bg-slate-300 my-1'>
 
-                                </li>
-                                <li className=' px-4 py-2 hover:bg-slate-300 my-1'>
-                                    <NavLink className={(isActive) => isActive ? 'text-xl text-gray-700 ' : '  text-xl text-black '} to='/dashboard/employee-list'>Employee List</NavLink>
+                                        <NavLink className={(isActive) => isActive ? 'text-xl text-gray-700 ' : '  text-xl text-black '} to='/dashboard/all-employee-list'>All Employee</NavLink>
+                                    </li>
+                                    <li className=' px-4 py-2 hover:bg-slate-300 my-1'>
+                                        <NavLink className={(isActive) => isActive ? 'text-xl text-gray-700 ' : '  text-xl text-black '} to='/dashboard/payroll'>Pay Role</NavLink>
 
-                                </li>
-                                <li className=' px-4 py-2 hover:bg-slate-300 my-1'>
-                                    <NavLink className={(isActive) => isActive ? 'text-xl text-gray-700 ' : '  text-xl text-black '} to='/dashboard/progress'>Progress</NavLink>
+                                    </li>
+                                </>}
+                                {/* just Hr */}
+                                {role === 'HR' && <>
+                                    <li className=' px-4 py-2 hover:bg-slate-300 my-1'>
+                                        <NavLink className={(isActive) => isActive ? 'text-xl text-gray-700 ' : '  text-xl text-black '} to='/dashboard/employee-list'>Employee List</NavLink>
 
-                                </li>
+                                    </li>
+                                    <li className=' px-4 py-2 hover:bg-slate-300 my-1'>
+                                        <NavLink className={(isActive) => isActive ? 'text-xl text-gray-700 ' : '  text-xl text-black '} to='/dashboard/progress'>Progress</NavLink>
+
+                                    </li>
+                                </>}
                                 {/* just emloyee */}
-                                {user && <>
+                                {role === 'Employee' && <>
                                     <li className=' px-4 py-2 hover:bg-slate-300 my-1'>
 
                                         <NavLink className={(isActive) => isActive ? 'text-xl text-gray-700 ' : '  text-xl text-black '} to='/dashboard/work-sheet'>Work Shit</NavLink>
